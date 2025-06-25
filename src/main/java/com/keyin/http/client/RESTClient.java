348c3f585f8bd89bfaac3cbd4eea4b82e89988a1
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-06-25 18:50:29 -0230

    Added get requests for each question

--- src/main/java/com/keyin/http/client/RESTClient.java
+++ src/main/java/com/keyin/http/client/RESTClient.java
@@ -41,6 +41,110 @@ public class RESTClient {
         return responseBody;
     }
 
+    public List<Airport> getAirportsByCity() {
+        List<Airport> airports = new ArrayList<Airport>();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
+
+        try {
+            HttpResponse<String> httpResponse = getHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if (httpResponse.statusCode() == 200) {
+                System.out.println("***** " + httpResponse.body());
+            } else {
+                System.out.println("Error Status Code: " + httpResponse.statusCode());
+            }
+
+            airports = buildAirportListFromHttpResponse(httpResponse.body());
+        } catch (IOException | InterruptedException error) {
+            error.printStackTrace();
+        }
+
+        return airports;
+    }
+
+    public List<Aircraft> getAircraftByPassenger() {
+        List<Aircraft> aircrafts = new ArrayList<Aircraft>();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
+
+        try {
+            HttpResponse<String> httpResponse = getHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if (httpResponse.statusCode() == 200) {
+                System.out.println("***** " + httpResponse.body());
+            } else {
+                System.out.println("Error Status Code: " + httpResponse.statusCode());
+            }
+
+            aircrafts = buildAircraftListFromHttpResponse(httpResponse.body());
+        } catch (IOException | InterruptedException error) {
+            error.printStackTrace();
+        }
+
+        return aircrafts;
+    }
+
+    public List<Airport> getAirportsByAircraft() {
+        List<Airport> airports = new ArrayList<Airport>();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
+
+        try {
+            HttpResponse<String> httpResponse = getHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if (httpResponse.statusCode() == 200) {
+                System.out.println("***** " + httpResponse.body());
+            } else {
+                System.out.println("Error Status Code: " + httpResponse.statusCode());
+            }
+
+            airports = buildAirportListFromHttpResponse(httpResponse.body());
+        } catch (IOException | InterruptedException error) {
+            error.printStackTrace();
+        }
+
+        return airports;
+    }
+
+    public List<Airport> getAirportsByPassengers() {
+        List<Airport> airports = new ArrayList<Airport>();
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(serverURL)).build();
+
+        try {
+            HttpResponse<String> httpResponse = getHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if (httpResponse.statusCode() == 200) {
+                System.out.println("***** " + httpResponse.body());
+            } else {
+                System.out.println("Error Status Code: " + httpResponse.statusCode());
+            }
+
+            airports = buildAirportListFromHttpResponse(httpResponse.body());
+        } catch (IOException | InterruptedException error) {
+            error.printStackTrace();
+        }
+
+        return airports;
+    }
+
+    public List<Airport> buildAirportListFromHttpResponse(String httpResponse) throws JsonProcessingException {
+        List<Airport> airports = new ArrayList<Airport>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        airports = objectMapper.readValue(httpResponse, new TypeReference<List<Airport>>(){});
+
+        return airports;
+    }
+
+    public List<Aircraft> buildAircraftListFromHttpResponse(String httpResponse) throws JsonProcessingException {
+        List<Aircraft> aircrafts = new ArrayList<Aircraft>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        aircrafts = objectMapper.readValue(httpResponse, new TypeReference<List<Aircraft>>(){});
+
+        return aircrafts;
+    }
+
     public HttpClient getHttpClient() {
         if (httpClient == null) {
             httpClient = HttpClient.newHttpClient();
