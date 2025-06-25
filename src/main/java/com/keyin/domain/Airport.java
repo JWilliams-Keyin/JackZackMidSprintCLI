3457a2555c776f0f7cef017bcc0d58ddb2f19147
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-06-25 16:01:23 -0230

    Added all POJOs to new domain folder

---
+++ src/main/java/com/keyin/domain/Airport.java
@@ -0,0 +1,55 @@
+package com.keyin.domain;
+
+public class Airport {
+    private long id;
+
+    public String airportName;
+    public String airportCode;
+    private City airportCity;
+
+    public Airport() {
+
+    }
+
+    public Airport(long id) {
+        this.id = id;
+    }
+
+    public Airport(String airportName, String airportCode) {
+        this.airportName = airportName;
+        this.airportCode = airportCode;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getAirportName() {
+        return airportName;
+    }
+
+    public String getAirportCode() {
+        return airportCode;
+    }
+
+    public City getAirportCity() {
+        return airportCity;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setAirportName(String airportName) {
+        this.airportName = airportName;
+    }
+
+    public void setAirportCode(String airportCode) {
+        this.airportCode = airportCode;
+    }
+
+    public void setAirportCity(City airportCity) {
+        this.airportCity = airportCity;
+    }
+}
+
