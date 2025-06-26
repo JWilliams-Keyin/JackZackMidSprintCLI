0df3e3505d9c7bd764790e847c2bca5c0f800341
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-06-26 17:27:01 -0230

    Reverted past 2 commits

--- src/test/java/com/keyin/client/RESTClientTest.java
+++ src/test/java/com/keyin/client/RESTClientTest.java
@@ -14,17 +14,4 @@ import static org.mockito.Mockito.*;
 
 public class RESTClientTest {
 
-    private RESTClient restClient;
-    private HttpClient mockHttpClient;
-    private HttpResponse<String> mockResponse;
-
-    @BeforeEach
-    public void setUpTestRestClient() {
-        restClient = new RESTClient();
-
-        mockHttpClient = mock(HttpClient.class);
-        mockResponse = mock(HttpResponse.class);
-
-        restClient.setHttpClient(mockHttpClient);
-    }
 }
