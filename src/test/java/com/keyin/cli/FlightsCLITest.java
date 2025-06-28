44a468715dd3c5a9b0fcc6719901b6d5a977f16d
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-06-28 13:25:51 -0230

    Fixed problem in FlightsCLITest.java caused by last commit

--- src/test/java/com/keyin/cli/FlightsCLITest.java
+++ src/test/java/com/keyin/cli/FlightsCLITest.java
@@ -3,10 +3,12 @@ package com.keyin.cli;
 import com.keyin.domain.Aircraft;
 import com.keyin.domain.Airport;
 import com.keyin.domain.City;
+import com.keyin.domain.Passenger;
 import com.keyin.http.client.RESTClient;
 import com.keyin.http.cli.FlightsCLI;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -35,13 +37,23 @@ public class FlightsCLITest {
 
         @Override
         public List<Aircraft> getAircraftByPassenger() {
+            Passenger passenger1 = new Passenger("Test", "Man", "1234567890");
+            List<Passenger> passengerList1 = new ArrayList<>();
+            passengerList1.add(passenger1);
+
             Aircraft aircraft1 = new Aircraft();
             aircraft1.setAircraftType("TypeX");
             aircraft1.setAirlineName("AirlineX");
+            aircraft1.setAircraftPassengers(passengerList1);
+
+            Passenger passenger2 = new Passenger("Test", "Woman", "0987654321");
+            List<Passenger> passengerList2 = new ArrayList<>();
+            passengerList1.add(passenger2);
 
             Aircraft aircraft2 = new Aircraft();
             aircraft2.setAircraftType("TypeY");
             aircraft2.setAirlineName("AirlineY");
+            aircraft2.setAircraftPassengers(passengerList2);
 
             return Arrays.asList(aircraft1, aircraft2);
         }
